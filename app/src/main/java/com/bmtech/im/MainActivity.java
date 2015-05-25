5dc86908064b5b462245ba0665e10f639183e718
Lycoris <lycoris@outlook.com>
2015-05-25 15:51:55 +0800

    update project config

---
+++ app/src/main/java/com/bmtech/im/MainActivity.java
@@ -0,0 +1,139 @@
+package com.bmtech.im;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
+
+import com.github.florent37.materialviewpager.MaterialViewPager;
+import com.bmtech.im.fragment.RecyclerViewFragment;
+import com.bmtech.im.fragment.ScrollFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MaterialViewPager mViewPager;
+
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("");
+
+        mViewPager = (MaterialViewPager) findViewById(R.id.materialViewPager);
+
+        toolbar = mViewPager.getToolbar();
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+
+            final ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setDisplayShowHomeEnabled(true);
+                actionBar.setDisplayShowTitleEnabled(true);
+                actionBar.setDisplayUseLogoEnabled(false);
+                actionBar.setHomeButtonEnabled(true);
+            }
+        }
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, 0, 0);
+        mDrawer.setDrawerListener(mDrawerToggle);
+
+        mViewPager.getViewPager().setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+
+            int oldPosition = -1;
+
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    //case 0:
+                    //    return RecyclerViewFragment.newInstance();
+                    case 1:
+                        return ScrollFragment.newInstance();
+                    //case 2:
+                    //    return WebViewFragment.newInstance();
+                    default:
+                        return RecyclerViewFragment.newInstance();
+                }
+            }
+
+            @Override
+            public void setPrimaryItem(ViewGroup container, int position, Object object) {
+                super.setPrimaryItem(container, position, object);
+
+                //only if position changed
+                if (position == oldPosition)
+                    return;
+                oldPosition = position;
+
+                int color = 0;
+                String imageUrl = "";
+                switch (position) {
+                    case 0:
+                        imageUrl = "http://cdn1.tnwcdn.com/wp-content/blogs.dir/1/files/2014/06/wallpaper_51.jpg";
+                        color = getResources().getColor(R.color.blue);
+                        break;
+                    case 1:
+                        imageUrl = "https://fs01.androidpit.info/a/63/0e/android-l-wallpapers-630ea6-h900.jpg";
+                        color = getResources().getColor(R.color.green);
+                        break;
+                    case 2:
+                        imageUrl = "http://www.droid-life.com/wp-content/uploads/2014/10/lollipop-wallpapers10.jpg";
+                        color = getResources().getColor(R.color.cyan);
+                        break;
+                    case 3:
+                        imageUrl = "http://www.tothemobile.com/wp-content/uploads/2014/07/original.jpg";
+                        color = getResources().getColor(R.color.red);
+                        break;
+                }
+
+                final int fadeDuration = 400;
+                mViewPager.setImageUrl(imageUrl, fadeDuration);
+                mViewPager.setColor(color, fadeDuration);
+
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+                    case 0:
+                        return "Selection";
+                    case 1:
+                        return "Actualités";
+                    case 2:
+                        return "Professionnel";
+                    case 3:
+                        return "Divertissement";
+                }
+                return "";
+            }
+        });
+        mViewPager.getViewPager().setOffscreenPageLimit(mViewPager.getViewPager().getAdapter().getCount());
+        mViewPager.getPagerTitleStrip().setViewPager(mViewPager.getViewPager());
+
+        mViewPager.getViewPager().setCurrentItem(1);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+}
